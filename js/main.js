ee4add8e09f1a6ac52b4554475243b4334ee9554
tanji <tanjiadm@gmail.com>
2019-11-12 18:37:58 +0900

    classListを追加

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 'use strict'
 
 {
-    
+    const btn = document.getElementById('btn');
+    const white = document.getElementById('white');
+    const white2 = document.getElementById('white2');
+    const white3 = document.getElementById('white3');
+    btn.addEventListener('click', ()=> {
+        white.classList.add('white');
+    });
+    btn.addEventListener('click', ()=> {
+        white2.classList.add('white');
+    });
+    btn.addEventListener('click', ()=> {
+        white3.classList.add('white');
+    });
 }
